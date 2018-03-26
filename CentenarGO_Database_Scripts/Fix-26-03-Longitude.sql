2cf2502ccc8c5a08af71de9df7198f9b4f25ed55
tpr <tudorpetreradu@gmail.com>
2018-03-26 11:54:57 +0300

    Added two new endpoints

---
+++ CentenarGO_Database_Scripts/Fix-26-03-Longitude.sql
@@ -0,0 +1,2 @@
+ALTER TABLE landmarks DROP COLUMN longitute;
+ALTER TABLE landmarks ADD longitude REAL NOT NULL;
--- CentenarGO_Server/api.js
+++ CentenarGO_Server/api.js
@@ -17,8 +17,9 @@ const router = express.Router();
 router.post('/signup', (req, res) => {
     bcrypt.hash(req.body.password, 10)
         .then((hash) => {
-            client.query('INSERT INTO users (username, email, password) VALUES ($1::text, $2::text, $3::text)',
-                        [req.body.username, req.body.email, hash])
+            client.query(`INSERT INTO users (username, email, password) 
+                          VALUES ($1::text, $2::text, $3::text)`, 
+                          [req.body.username, req.body.email, hash])
                 .then(() => {
                     res.sendStatus(200);
                 })
@@ -26,6 +27,7 @@ router.post('/signup', (req, res) => {
                     console.log(err.stack);
                     if (err.message.toLowerCase().includes('duplicate')) {
                         res.status(400).send('Duplicate primary key in User table (email already exists).');
+                        return;
                     }
                     else {
                         res.sendStatus(500);
@@ -39,26 +41,31 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-
-    client.query('SELECT id as id, password AS hash FROM users WHERE email = $1::text',
-                 [req.body.email])
+    client.query(`SELECT id as id, password AS hash 
+                  FROM users 
+                  WHERE email = $1::text`, [req.body.email])
         .then((result) => {
-            if (result.rows.length > 0) {
-                hash = result.rows[0].hash;
-                bcrypt.compare(req.body.password, hash)
-                    .then((same) => {
-                        res.status(200);
-                        let token = jwt.sign({data: result.rows[0].id}, config.tokenSecret, {expiresIn: 30 * 24 * 60});
-                        res.json({token: token});
-                    })
-                    .catch((err) => {
-                        console.log(err.stack);
-                        res.sendStatus(401);
-                    });
-            }
-            else {
+            if (result.rows.length === 0) {
                 res.status(404).send('User does not exist.');
+                return;
             }
+            hash = result.rows[0].hash;
+            bcrypt.compare(req.body.password, hash)
+                .then((same) => {
+                    res.status(200);
+                    jwt.sign({id: result.rows[0].id}, config.tokenSecret, {expiresIn: 30 * 24 * 60}, (err, token) => {
+                        if (err) {
+                            console.log(err.stack);
+                            res.sendStatus(500);
+                            return;
+                        }
+                        res.json({token: token});
+                    });
+                })
+                .catch((err) => {
+                    console.log(err.stack);
+                    res.sendStatus(401);
+                });
         })
         .catch((err) => {
             console.log(err.stack);
@@ -73,15 +80,59 @@ router.use((req, res, next) => {
     if (token) {
         jwt.verify(token, config.tokenSecret, (err, decoded) => {
             if (err) {
-                res.sendStatus(403);
+                if (err.message.toLowerCase().includes('expired')) {
+                    res.status(401).send('Token expired');
+                    return;
+                }
+                res.sendStatus(401);
+                return;
             } 
-            req.decoded = decoded;
+            req.id = decoded.id;
             next();
         });
     }
     else {
-        res.sendStatus(403);
+        res.sendStatus(401);
     }
+});
+
+router.post('/routes', (req, res, next) => {
+    client.query(`SELECT r.id as id, r.name as name, l.latitude as latitude, l.longitude as longitude
+                  FROM routes r
+                  JOIN landmarks l ON l.route = r.id
+                  JOIN userroutes ur ON ur.routeid = r.id
+                  JOIN users u ON ur.userid = u.id
+                  WHERE u.id = $1::uuid
+                  AND l.routeorder = 1`, [req.id])
+        .then((result) => {
+            if (result.rows.length === 0) {
+                res.status(500).send('User has no routes');
+                return;
+            }
+            res.status(200).json({routes: result.rows});
+        })
+        .catch((err) => {
+            console.log(err.stack);
+            res.sendStatus(500);
+        });
+});
+
+router.post('/route/:routeId', (req, res, next) => {
+    client.query(`SELECT r.name as name, l.latitude as latitude, l.longitude as longitude
+                  FROM routes r
+                  JOIN landmarks l ON l.route = r.id
+                  WHERE r.id = $1::int`, [req.params.routeId])
+        .then((result) => {
+            if (result.rows.length === 0) {
+                res.status(500).send('Route has no landmarks');
+                return;
+            }
+            res.status(200).json({routes: result.rows});
+        })
+        .catch((error) => {
+            console.log(err.stack);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = {
