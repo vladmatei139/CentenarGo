c907bacbc8e1e42355f7668e63f18d57522d3fa8
tpr <tudorpetreradu@gmail.com>
2018-03-28 14:56:28 +0300

    Await syntax, fixes, endpoint for chaning current route

--- CentenarGO_Server/api.js
+++ CentenarGO_Server/api.js
@@ -14,6 +14,10 @@ const bcrypt = require('bcrypt');
 
 const router = express.Router(); 
 
+const errorCatcher = fn => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next); 
+};
+
 router.post('/signup', (req, res) => {
     bcrypt.hash(req.body.password, 10)
         .then((hash) => {
@@ -96,92 +100,127 @@ router.use((req, res, next) => {
     }
 });
 
-router.post('/routes', (req, res, next) => {
+router.post('/routes', errorCatcher(async (req, res, next) => {
     /**
      * Rutele sunt aceleasi pentru toti userii logati. 
      * Se intorc numele "name" si coordonatele ("beginLatitude", "beginLongitude") ale primului obiectiv al fiecarei rute care are cel putin un obiectiv.
      */
-    client.query(`SELECT r.id as id, r.name as name, l.latitude as beginLatitude, l.longitude as beginLongitude
-                  FROM routes r
-                  JOIN landmarks l ON l.route = r.id
-                  WHERE l.routeorder = 1`)
-        .then((result) => {
-            if (result.rows.length === 0) {
-                res.status(500).send('User has no routes');
-                return;
-            }
-            res.status(200).json({routes: result.rows});
-        })
-        .catch((err) => {
-            console.error(err.stack);
-            res.sendStatus(500);
-        });
-});
+    const { rows } = await client.query(`SELECT r.id as id, r.name as name, l.latitude as beginLatitude, l.longitude as beginLongitude
+                                         FROM routes r
+                                         JOIN landmarks l ON l.route = r.id
+                                         WHERE l.routeorder = 1`);
+    if (rows.length === 0) {
+        res.status(500).send('No routes.');
+        return;
+    }
+    res.status(200).json({routes: rows});
+}));
 
-router.post('/route/:routeId/landmarks', (req, res, next) => {
+router.post('/route/:routeId/', errorCatcher(async (req, res, next) => {
     /**
      * Se extrag obiectivele pentru ruta ceruta.
      * Daca ruta ceruta nu este cea curenta, se intoarce doar primul obiectiv.
      * Altfel, se intorc toate obiectivele pana la cel curent inclusiv.
      */
-    client.query(`(WITH tbl1 AS 
-                   (
-                    SELECT l.id AS id, l.name AS name, l.latitude AS latitude, l.longitude AS longitude, l.routeorder AS routeorder
-                    FROM landmarks l
-                    JOIN routes r ON r.id = l.route
-                    JOIN userdetails ud ON r.id = ud.currentroute
-                    WHERE ud.userid = $1::uuid 
-                    AND r.id = $2::int
-                   )
-                   SELECT id, name, latitude, longitude, routeorder, (routeorder = (SELECT MAX(routeorder) FROM tbl1)) AS is_current
-                   FROM tbl1)
-                  UNION
-                  (SELECT l.id, l.name, l.latitude, l.longitude, l.routeorder, false as is_current 
-                   FROM landmarks l
-                   JOIN routes r ON r.id = l.route
-                   JOIN userdetails ud ON ud.userid = $1::uuid
-                   WHERE r.id = $2::int
-                   AND l.routeorder = 1)
-                  ORDER BY 5 ASC`, [req.id, req.params.routeId])
-        .then(result => {
-            if (result.rows.length === 0) {
-                res.status(500).send('Route has no landmarks');
-                return;
-            }
-            res.status(200).json({landmarks: result.rows});
-        })
-        .catch(err => {
-            console.error(err.stack);
-            res.sendStatus(500);
-        });
-});
+    const { rows } = await client.query(`(WITH tbl1 AS (
+                                            SELECT l.id AS id, l.name AS name, l.latitude AS latitude, l.longitude AS longitude, l.routeorder AS routeorder
+                                            FROM landmarks l
+                                            JOIN routes r ON r.id = l.route
+                                            JOIN userdetails ud ON r.id = ud.currentroute
+                                            WHERE ud.userid = $1::uuid 
+                                            AND r.id = $2::int
+                                            AND l.routeorder <= (
+                                                SELECT routeorder
+                                                FROM landmarks
+                                                JOIN userroutes ON landmarks.id = userroutes.currentlandmark 
+                                                WHERE userid = $1::uuid
+                                                AND routeid = $2::int
+                                            )
+                                          )
+                                          SELECT id, name, latitude, longitude, routeorder, (routeorder = (SELECT MAX(routeorder) FROM tbl1)) AS is_current
+                                          FROM tbl1
+                                         )
+                                         UNION (
+                                          SELECT l.id, l.name, l.latitude, l.longitude, l.routeorder, false as is_current 
+                                          FROM landmarks l
+                                          JOIN routes r ON r.id = l.route
+                                          JOIN userdetails ud ON r.id <> ud.currentroute
+                                          WHERE ud.userid = $1::uuid 
+                                          AND r.id = $2::int
+                                          AND l.routeorder = 1
+                                         )
+                                         ORDER BY 5 ASC`, [req.id, req.params.routeId]);
+    if (rows.length === 0) {
+        res.status(500).send('Route has no landmarks or does not exist.');
+        return;
+    }
+    res.status(200).send({landmarks: rows});
+}));
 
-router.post('/landmark/:landmarkId', (req, res, next) => {
+router.post('/landmark/:landmarkId', errorCatcher(async (req, res, next) => {
     /**
      * Se intoarce obiectivul cerut din ruta curenta, cu toate datele din tabel.
      * Daca obiectivul este cel curent, content-ul se trunchiaza la 100 de caractere.
      */
-    client.query(`SELECT l.id, l.name, l.content, l.route, l.latitude, l.longitude, l.routeorder, LEAST(1, COALESCE(ur.currentlandmark, 0)) as is_current
-                  FROM landmarks l
-                  JOIN routes r ON r.id = l.route
-                  JOIN userdetails ud ON ud.currentroute = r.id 
-                  LEFT JOIN userroutes ur ON ur.currentlandmark = l.id
-                  WHERE ud.userid = $1::uuid 
-                  AND l.id = $2::int`, [req.id, req.params.landmarkId])
-        .then(result => {
-            if (result.rows.length === 0) {
-                res.status(500).send('Landmark does not exist or is not part of the current route.');
-                return;
-            }
-            landmark = result.rows[0];
-            landmark.is_current = landmark.is_current > 0;
-            if (landmark.is_current) {
-                landmark.content = landmark.content.substr(0, Math.min(landmark.content.length, 100));
-            }
-            res.status(200).json({landmark: landmark});
+    const { rows } = await client.query(`SELECT l.id, l.name, l.content, l.route, l.latitude, l.longitude, l.routeorder, LEAST(1, COALESCE(ur.currentlandmark, 0)) as is_current
+                                         FROM landmarks l
+                                         JOIN routes r ON r.id = l.route
+                                         JOIN userdetails ud ON ud.currentroute = r.id 
+                                         LEFT JOIN userroutes ur ON ur.currentlandmark = l.id
+                                         WHERE ud.userid = $1::uuid 
+                                         AND l.id = $2::int
+                                         AND l.routeorder <= (
+                                             SELECT routeorder
+                                             FROM landmarks
+                                             JOIN userroutes ON landmarks.route = userroutes.routeid 
+                                             WHERE id = userroutes.currentlandmark
+                                             AND userid = $1::uuid)`, [req.id, req.params.landmarkId]);
+    if (rows.length === 0) {
+        res.status(400).send('Landmark part of the current route, but more advanced than the current landmark, is not part of the current route, or does not exist.');
+        return;
+    }
+    landmark = rows[0];
+    landmark.is_current = landmark.is_current > 0;
+    if (landmark.is_current) {
+        landmark.content = landmark.content.substr(0, Math.min(landmark.content.length, 100));
+    }
+    res.status(200).json({landmark: landmark});
+}));
+
+router.post('/route/change/:routeId', errorCatcher(async (req, res, next) => {
+    /**
+     * Schimba ruta curenta.
+     */
+    await client.query('BEGIN');
+    await client.query(`UPDATE userroutes
+                        SET routeid = $2::int,
+                            currentlandmark = (
+                                SELECT id
+                                FROM landmarks
+                                WHERE route = $2::int
+                                AND routeorder = 1
+                            )
+                        WHERE userid = $1::uuid
+                        AND routeid = (
+                            SELECT currentroute
+                            FROM userdetails
+                            WHERE userid = $1::uuid)`, [req.id, req.params.routeId]);
+    await client.query(`UPDATE userdetails
+                        SET currentroute = $2::int
+                        WHERE userid = $1::uuid`, [req.id, req.params.routeId]);
+    await client.query('COMMIT');
+    res.sendStatus(200);
+}));
+
+router.use((err, req, res, next) => {
+    console.error(err);
+    client.query('ROLLBACK')
+        .then(() => {
+            res.sendStatus(500);
         })
         .catch(err => {
-            console.error(err.stack);
+            console.error(err);
+            //ROLLBACK error... R.I.P.
             res.sendStatus(500);
         });
 });
