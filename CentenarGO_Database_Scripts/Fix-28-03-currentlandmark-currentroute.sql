7bd254b6f0c23902c25e648cce4d68211832b19d
vladmatei139@gmail.com <vladmatei139@gmail.com>
2018-03-29 13:56:59 +0300

    Uploaded script for mock-data; Automatically resets table data and sequences; Run with: psql -U username -d database -a -f path/to/Mock-data.sql

--- CentenarGO_Database_Scripts/Fix-28-03-currentlandmark-currentroute.sql
+++ CentenarGO_Database_Scripts/Fix-28-03-currentlandmark-currentroute.sql
@@ -1,4 +1,4 @@
-ALTER TABLE userroutes ADD CONSTRAINT userroutes_currentlandmark_fk FOREIGNKEY (currentlandmark) REFERENCES landmarks(id);
+ALTER TABLE userroutes ADD CONSTRAINT userroutes_currentlandmark_fk FOREIGN KEY (currentlandmark) REFERENCES landmarks(id);
 ALTER TABLE userroutes ALTER currentlandmark SET NOT NULL;
 
 ALTER TABLE userdetails ADD CONSTRAINT userdetails_currentroute_fk FOREIGN KEY (currentroute) REFERENCES routes(id);
